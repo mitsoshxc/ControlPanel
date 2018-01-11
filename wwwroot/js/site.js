b63e176612f36417fb25eb87d27fff7cf99c0f3c
mitsoshxc <mitsoshxc@gmail.com>
2018-01-11 15:26:18 +0200

    Edit customer form js validation added

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,16 +1,18 @@
 ﻿// Write your JavaScript code.
-// Add customer for validation
-function AddCustValidate()
-{
+// Customer forms validation
+//
+// Add
+//
+function AddCustValidate() {
     var _res = true;
 
     document.getElementById("customer_name").style.display = "none";
-    
-    if (document.forms["add_customer"]["_name"].value == "") {
-        document.getElementById("customer_name").innerHTML = "Name can not be empty!";
+
+    if (document.forms["customer"]["_name"].value == "") {
+        document.getElementById("customer_name").innerHTML = "Customer's name can not be empty!";
         document.getElementById("customer_name").style.display = "block";
         _res = false;
     }
-    
+
     return _res;
 }
