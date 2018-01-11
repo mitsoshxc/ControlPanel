6525477e51d2e52e6b18bc87500390dab330d691
mitsoshxc <mitsoshxc@gmail.com>
2018-01-11 14:34:27 +0200

    Add customer form js validation added

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,16 @@
 ﻿// Write your JavaScript code.
+// Add customer for validation
+function AddCustValidate()
+{
+    var _res = true;
+
+    document.getElementById("customer_name").style.display = "none";
+    
+    if (document.forms["add_customer"]["_name"].value == "") {
+        document.getElementById("customer_name").innerHTML = "Name can not be empty!";
+        document.getElementById("customer_name").style.display = "block";
+        _res = false;
+    }
+    
+    return _res;
+}
